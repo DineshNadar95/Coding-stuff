70999f1751902741bdc17a3da140a5bb12787ab0
Mishal Shah <mshah31@asu.edu>
2020-08-26 23:53:25 -0700

    added bfs

--- Educative/Graphs/BFS.java
+++ Educative/Graphs/BFS.java
@@ -168,18 +168,19 @@ class BFS {
 	public static void main(String[] args) {
 		
 		Set<Integer> visited = new HashSet<>();
-		int vertices = 5;
+		int vertices = 7;
 		GraphRep obj = new GraphRep(vertices); 
-		obj.addEdge(0,1);
-		obj.addEdge(0,2);
-		obj.addEdge(1,4);
+		obj.addEdge(1,2);
 		obj.addEdge(1,3);
+		obj.addEdge(2,5);
+		obj.addEdge(2,4);
+		obj.addEdge(3,6);
 		obj.print();
 		
 		String result = "";
 
 		// Write - Your - Code
-		
+		// BFS
 		Queue<Integer> queue = new LinkedList<>();
 		queue.offer(0);
 		
@@ -199,6 +200,30 @@ class BFS {
 			}
 		}
 		
-		System.out.println(result); 
+		System.out.println("BFS: "+result); 
+		
+		result = "";
+		
+		// DFS
+		Set<Integer> visited2 = new HashSet<>();
+		Stack<Integer> stack = new Stack<>();
+		stack.push(1);
+		
+		while(!stack.isEmpty()){
+			int source = stack.pop();
+			result += String.valueOf(source);
+			visited2.add(source);
+			// get headNode corresponding to this vertex number
+			// need to cover all the vertex numbers
+			DoublyLinkedList<Integer>.Node destination = obj.adjacencyList[source].getHeadNode();
+			while(destination != null){
+				if(!visited2.contains(destination.data)){
+					stack.push(destination.data);
+				}
+				destination = destination.nextNode;
+			}
+		}
+		
+		System.out.println("DFS: "+result);
 	}
 }
