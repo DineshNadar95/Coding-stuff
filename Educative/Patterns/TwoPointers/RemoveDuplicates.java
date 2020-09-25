bfd0b7925ef2763c05d9d97ddea3a23ee5c3f703
Mishal Shah <mshah31@asu.edu>
2020-09-24 22:33:01 -0700

    Variant 2

--- Educative/Patterns/TwoPointers/RemoveDuplicates.java
+++ Educative/Patterns/TwoPointers/RemoveDuplicates.java
@@ -12,5 +12,20 @@ class RemoveDuplicates {
 		}
 		
 		System.out.println(nextNonDuplicate);
+		
+		
+		// variant 2
+		int[] input2 = new int[]{3, 2, 3, 6, 3, 10, 9, 3};
+		int key = 3;
+		
+		int size = 0;
+		
+		for(int i=0; i<input2.length; i++){
+			if(input2[i] != key)
+				input2[size++] = input2[i];
+		}
+		
+		for(int i=0; i<size; i++)
+			System.out.print(input2[i]+"\t");
 	}
 }
